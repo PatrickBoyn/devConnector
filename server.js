97f3616891a6f96a6c23b5ca4778cb50042560fe
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-01-04 12:55:17 -0500

    Had this in the wrong place.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const connectDB = require('./config/db')
-const db = config.get('mongoURI', {
-  useNewUrlParser: true
-})
+
 const app = express()
 
-connectDB(db)
+connectDB()
 
 app.get('/', (req, res) => res.send('API Running.'))
 
