8b38856049c5d09434d987f626de3bc3147c567b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-01-06 13:58:47 -0500

    Added login functionality (ability to get a jwt)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
-
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 const User = require('../../models/User')
 
 // @route GET api/auth
@@ -16,5 +19,61 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error.')
   }
 })
+
+// @route POST api/ auth
+// @desc login user & get token.
+// @access Public
+router.post(
+  '/',
+  [
+    check('email', 'Please  include a valid email address.').isEmail(),
+    check('password', 'Password is required.').exists()
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+
+      if (!user) {
+        res.status(400).json({ errors: [{ message: 'User not found.' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'User not found.' }] })
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        // TODO change to 3600 seconds.
+        { expiresIn: 86400 },
+        (error, token) => {
+          if (error) throw error
+
+          res.json({ token })
+        }
+      )
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server error.')
+    }
+  }
+)
 
 module.exports = router
