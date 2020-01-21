9b3c0b12db68ae46342f89b7368691e2383714fa
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-01-21 13:48:13 -0500

    Added routing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/laoyouts/Navbar";
 import "./App.css";
 import Landing from "./components/laoyouts/Landing";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+    </Fragment>
+  </Router>
 );
 
 export default App;
