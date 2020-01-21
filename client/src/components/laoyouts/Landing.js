2d9af7a03b5e480f3592c0ff7e69774e2fb31c16
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-01-21 15:05:52 -0500

    Changed the export import style.

--- client/src/components/laoyouts/Landing.js
+++ client/src/components/laoyouts/Landing.js
@@ -1,27 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Landing = () => {
+export const Landing = () => {
   return (
-    <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
-          <p className="lead">
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Connector</h1>
+          <p className='lead'>
             Create developer profile/portfolio. Share posts, and get help from
             other developers
           </p>
-          <div className="button">
-            <a href="register.html" className="btn btn-primary">
+          <div className='button'>
+            <Link to='/register' className='btn btn-primary'>
               Register
-            </a>
-            <a href="login.html" className="btn">
+            </Link>
+            <Link to='/login' className='btn'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
     </section>
   );
 };
-
-export default Landing;
