17138beea6036dd2e6a6f23595f5d698e7d5b8cc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-02 21:53:13 -0500

    Created a use effect that dispatched the loaded user. Also added a token check.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, {Fragment, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/laoyouts/Navbar';
 import './App.css';
@@ -8,8 +8,20 @@ import Register from './components/auth/Register';
 import { Provider } from 'react-redux';
 import store from './store';
 import Alert from './components/laoyouts/Alert';
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 
-const App = () => (
+if(localStorage.token){
+    setAuthToken(localStorage.token);
+}
+
+const App = () => {
+
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
+
+    return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -25,6 +37,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
