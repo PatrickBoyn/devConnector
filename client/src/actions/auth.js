a7f209fb0588dac023cd6c3563e3fae39bd9891d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2020-02-04 16:55:35 -0500

    Dispatched loadUser, created a login method.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import { setAlert } from "./alert";
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL, USER_LOADED, AUTH_ERROR
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS
 } from './types';
 import setAuthToken from "../utils/setAuthToken";
 
@@ -42,6 +46,8 @@ export const register  = ({ name, email, password }) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         });
+
+        dispatch(loadUser());
     } catch(error) {
         const errors = error.response.data.errors;
 
@@ -51,5 +57,36 @@ export const register  = ({ name, email, password }) => async dispatch => {
         dispatch({
             type: REGISTER_FAIL
         });
+    }
+};
+
+// Login user
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+    } catch(error){
+        const errors = error.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.message, 'danger')))
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
+        })
     }
 };
